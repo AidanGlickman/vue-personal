f3886fcbf567489452af2ef18c3cb7eb709e705f
Aidan Glickman <aidan@aidanglickman.com>
2020-02-19 00:31:09 -0500

    clarify language in resume

--- src/components/pieces/resume/Skills.vue
+++ src/components/pieces/resume/Skills.vue
@@ -1,24 +1,27 @@
 <template>
   <div>
-    <h2>Skills</h2>
+    <h2>CS Skills</h2>
     <b-row>
-      <b-col sm="4" lg="3" v-for="skill in skills" :key="skill">{{skill}}</b-col>
-      <b-col sm="12" class="text-center">Ability to learn new technologies and skills very fast</b-col>
+      <b-col sm="4" lg="3" v-for="skill in skills" :key="skill">{{
+        skill
+      }}</b-col>
+      <b-col sm="12" class="text-center"
+        >Ability to learn new technologies and skills very fast</b-col
+      >
     </b-row>
   </div>
 </template>
 
 <script>
-import skillList from "@/assets/data/cskills.json";
-export default {
-  name: "Skills",
-  data() {
-    return {
-      skills: skillList
-    };
+  import skillList from '@/assets/data/cskills.json'
+  export default {
+    name: 'Skills',
+    data() {
+      return {
+        skills: skillList,
+      }
+    },
   }
-};
 </script>
 
-<style>
-</style>
+<style></style>
