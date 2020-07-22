fe6c307ea2697e564dd6ef9d7f72bb1e417a607d
Aidan Glickman <aidan@aidanglickman.com>
2020-07-21 21:41:28 -0400

    update resume

--- src/components/pieces/resume/Intro.vue
+++ src/components/pieces/resume/Intro.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <h3>Aidan Glickman</h3>
-    <a href="tel:+12012892065"><p>201 289 2065</p></a>
-    <a href="mailto:aidan@aidanglickman.com"><p>aidan@aidanglickman.com</p></a>
+    <a href="mailto:contact@aidanglickman.com">
+      <p>contact@aidanglickman.com</p>
+    </a>
   </div>
 </template>
 
 <script>
-  export default {}
+export default {};
 </script>
 
 <style></style>
